bb68c4283864ad5eb3a3ea38c1653fa237f9900b
SiMi723 <simi.20230090@mnnit.ac.in>
2025-04-11 21:46:16 +0530

    Modified

--- Binary Search/BS on 1D Arrays/Ceil_the_Floor.cpp
+++ Binary Search/BS on 1D Arrays/Ceil_the_Floor.cpp
@@ -2,25 +2,63 @@
 
 class Solution {
     public:
+       
+       int findFloor(int x, vector<int> &arr) {
+        int l=0; r = arr.size()-1;
+        int floor = -1;
+
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+
+            if(arr[mid] <= x) {
+                floor = arr[mid];
+                l = mid+1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return floor;
+       }
+
+       int findCeil(int x, vector<int> &arr) {
+        int l = 0, r = arr.size()-1;
+        int ceil = -1;
+
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+
+            if (arr[mid]>=x){
+                ceil = arr[mid];
+                r = mid -1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ceil;
+       }
+       
        vector<int> getFloorAndCeil(int x, vector<int> &arr) {
 
         // code here 
-        int floor = -1, ceil = -1;
+        // int floor = -1, ceil = -1;
 
-        int maxFloor = INT_MIN, minCeil = INT_MAX;
+        // int maxFloor = INT_MIN, minCeil = INT_MAX;
 
-        for(int num : arr) {
-            if (num <= x && num >= maxFloor) {
-                maxFloor = num;
-                floor = num;
-            }
+        // for(int num : arr) {
+        //     if (num <= x && num >= maxFloor) {
+        //         maxFloor = num;
+        //         floor = num;
+        //     }
 
-            if(num >= x && num < minCeil) {
-                minCeil = num;
-                ceil = num;
-            }
-        }
+        //     if(num >= x && num < minCeil) {
+        //         minCeil = num;
+        //         ceil = num;
+        //     }
+        // }
 
+        sort(arr.begin(), arr.end());
+        int floor = findFloor(x, arr);
+        int ceil = findCeil(x, arr);
         return {floor, ceil};
     }
 };
